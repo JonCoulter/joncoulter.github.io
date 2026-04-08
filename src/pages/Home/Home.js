071aee547aca183601c3f2b38888560456bab142
JonCoulter <jc803jc803@gmail.com>
2026-04-08 14:06:05 -0400

    added skeleton images

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Grid, Skeleton, Typography } from "@mui/material";
 import { Article } from "@mui/icons-material";
 
 import CustomButton from '../../components/CustomButton/CustomButton'
@@ -9,12 +9,32 @@ import './Home.css'
 import db from '../../utils/db'
 
 export default function Home(){
+    const [mobileImageLoaded, setMobileImageLoaded] = useState(false)
+    const [loadedExperienceImages, setLoadedExperienceImages] = useState({})
+
+    const markExperienceLoaded = (index) => {
+        setLoadedExperienceImages((prev) => ({
+            ...prev,
+            [index]: true,
+        }))
+    }
 
     return(
         <>
             <div className='home-mobile-profile container-shadow'>
                 <div className='home-mobile-profile-header'>
-                    <img className='home-mobile-profile-image' src={'/images/pfp.png'} alt='' />
+                    <div className='home-mobile-profile-image-wrapper'>
+                        {!mobileImageLoaded && (
+                            <Skeleton variant='rounded' animation='wave' className='home-mobile-profile-image-skeleton' />
+                        )}
+                        <img
+                            className={`home-mobile-profile-image ${mobileImageLoaded ? 'is-visible' : 'is-hidden'}`}
+                            src={'/images/pfp.png'}
+                            alt='Profile'
+                            onLoad={() => setMobileImageLoaded(true)}
+                            onError={() => setMobileImageLoaded(true)}
+                        />
+                    </div>
                     <div>
                         <Typography className='home-mobile-profile-name'>
                             {db.name}
@@ -59,10 +79,21 @@ export default function Home(){
 
             <Grid item xs={12}>
                 <Grid container spacing={3} justifyContent='space-around'>
-                    {db.about_experiences.map((experience) => (
-                        <Grid item xs={4} md={3}>
+                    {db.about_experiences.map((experience, index) => (
+                        <Grid item xs={4} md={3} key={`${experience.title}-${index}`}>
                             <div className='experience'>
-                                <img className='experience-image' src={experience.image}/>
+                                <div className='experience-image-wrapper'>
+                                    {!loadedExperienceImages[index] && (
+                                        <Skeleton variant='rounded' animation='wave' className='experience-image-skeleton' />
+                                    )}
+                                    <img
+                                        className={`experience-image ${loadedExperienceImages[index] ? 'is-visible' : 'is-hidden'}`}
+                                        src={experience.image}
+                                        alt={experience.title}
+                                        onLoad={() => markExperienceLoaded(index)}
+                                        onError={() => markExperienceLoaded(index)}
+                                    />
+                                </div>
                                 <Typography variant='body2' className='experience-title'>
                                     {experience.title}
                                 </Typography>
